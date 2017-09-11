b5e0f67a7d6ac4ad462d9b4bf815505032ad4e1f
Stoo <stoo.goff@gmail.com>
2017-09-11 12:11:36 +0100

    #2 Added config menu to tabs.

--- src/media/js/components/tabs.jsx
+++ src/media/js/components/tabs.jsx
@@ -5,12 +5,30 @@ import * as CONFIG from "lib/config";
 import { parent } from "lib/utils";
 
 export default class Tabs extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			active: -1
+		};
+	}
+
 	add() {
 		dispatcher.dispatch(CONFIG.TAB_ADD);
 	}
 
 	remove(evt) {
+		console.log("remove")
+		console.log(evt)
 		dispatcher.dispatch(CONFIG.TAB_REMOVE, this._getIndex(evt.target));
+	}
+
+	showMenu(evt) {
+		let index = this._getIndex(evt.target);
+
+		this.setState({
+			active: index === this.state.active ? -1 : index
+		});
 	}
 
 	view(evt) {
@@ -33,9 +51,13 @@ export default class Tabs extends React.Component {
 		return <ul id="tabs">
 			{ tabs.map((t, i) => <li key={ i } className={ i == this.props.selectedIndex ? "active" : null } data-index={ i }>
 				<span className="btn" onClick={ this.view.bind(this) }>{ t }</span>
-				{ tabs.length > 1 ? <small className="btn tools" onClick={ this.remove.bind(this) }><svg><use href="#icon-cross"></use></svg></small>
-					: null
-				}
+				<small className="btn tools" onClick={ this.showMenu.bind(this) }><svg><use href="#icon-cog"></use></svg></small>
+
+				<ul className={ this.state.active ==  i ? "active" : "" }>
+					<li>{ t }</li>
+					<li>Colour</li>
+					<li onClick={ this.remove.bind(this) }>Remove <small className="btn tools"><svg><use href="#icon-cross"></use></svg></small></li>
+				</ul>
 			</li>) }
 			<li className="tools"><small className="btn" onClick={ this.add.bind(this) }><svg><use href="#icon-plus"></use></svg> Add</small></li>
 		</ul>;
