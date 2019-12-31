db407fc571b07f99c5db00b3b3511fce4aee798f
Stoo <stoo.goff@gmail.com>
2019-12-31 12:30:40 +0000

    Added custom weapons. Added character tagging to relate different stats to the same character. This fixes #27

--- csv/parse.js
+++ csv/parse.js
@@ -7,18 +7,19 @@ const csv = require("csv-parser");
 const ABILITIES = require("./data/abilities.json");
 const FORCE_POWERS = require("./data/force-powers.json");
 const WEAPONS = require("./data/weapons.json");
+const CUSTOM_WEAPONS = require("./data/custom-weapons.json");
 
-const args = process.argv.slice(2);
 
-if(args.length === 0) {
-	console.error("File name must be provided.");
-	process.exit();
-}
+const after = (f, t) => {
+	return () => {
+		if(--t == 0) {
+			f();
+		}
+	}
+};
 
-const INPUT = path.join("data", args[0]);
 
-let output = {};
-let outputKeys = [
+const outputKeys = [
 	"characteristics",
 	"derived",
 	"skills",
@@ -27,22 +28,23 @@ let outputKeys = [
 	"gear",
 	"tags"
 ];
-let chunk = null;
-let keyMap = {
+const keyMap = {
 	"Soak": "soak",
 	"Wound Threshold": "wounds",
 	"Strain Threshold": "strain"
 };
-let tagMap = {
-	"jedi.csv": ["jedi", "force sensitive"],
-	"republic.csv": ["republic"],
-	"separatist.csv": ["separatist"],
-	"scum.csv": ["underworld"],
+const tagMap = {
+	"bonus.csv": [],
 	"darkside.csv": ["darkside", "force sensitive"],
+	"jedi.csv": ["jedi", "force sensitive"],
+	"people.csv": [],
+	"republic.csv": ["republic"],
+	"scum.csv": ["underworld"],
+	"separatists.csv": ["separatist"]
 };
 
-// convert abilities and force powers to name/id as key
-let abilities = {};
+// convert custom weapons, abilities and force powers to name/id as key
+let abilities = {}, customWeapons = {};
 
 ABILITIES.forEach(a => abilities[a.name] = a);
 FORCE_POWERS.map(m => {
@@ -50,109 +52,130 @@ FORCE_POWERS.map(m => {
 
 	return m;
 }).forEach(a => abilities[a.id.toLowerCase()] = a);
+CUSTOM_WEAPONS.forEach(a => customWeapons[a.name] = a);
 
-fs.createReadStream(INPUT)
-	.pipe(csv())
-	.on("data", data => {
-		try {
-			if(data.Name == "Type") {
-				Object.keys(data).forEach(key => {
-					if(key != "Name") {
-						output[key] = {
-							"name": key,
-							"type": data[key],
-							"source": {
-								"owner": "Kualan"
-							},
-							"tags": ["source:Heroes on Both Sides", ...tagMap[args[0]]]
-						};
-					}
-				});
-			}
-			else if(outputKeys.indexOf(data.Name.toLowerCase()) != -1) {
-				chunk = data.Name.toLowerCase();
-			}
-			else {
-				Object.keys(data).forEach(key => {
-					if(key != "Name") {
-						if(!(chunk in output[key])) {
-							switch(chunk) {
-								case "gear":
-								case "talents":
-								case "abilities":
-									output[key][chunk] = [];
-									break;
 
-								// tags key has already been created
-								case "tags":
-									break;
+// get all of the CSV files ready to process
+const FILES = fs.readdirSync("input");
+//const FILES = ["bonus.csv"];
 
-								default:
-									output[key][chunk] = {};
-							}
+// output to
+let finalOutput = [];
+
+let complete = after(() => {
+	console.log(JSON.stringify(finalOutput, null, 1).trim());
+}, FILES.length);
+
+
+
+// process all files
+FILES.forEach(input => {
+	let output = {};
+	let chunk = null;
+	let file = path.join("input", input);
+
+	//console.log("", `Processing ${input}`, "")
+
+	// read each file and add everything
+	fs.createReadStream(file)
+		.pipe(csv())
+		.on("data", data => {
+			try {
+				if(data.Name == "Type") {
+					Object.keys(data).forEach(key => {
+						if(key != "Name") {
+							output[key] = {
+								"name": key,
+								"type": data[key],
+								"source": {
+									"owner": "Kualan"
+								},
+								"tags": ["source:Heroes on Both Sides", ...tagMap[input]]
+							};
 						}
-
-						if(data[key]) {
-							let name = data.Name in keyMap ? keyMap[data.Name] : data.Name;
-
-							switch(chunk) {
-								case "gear":
-								case "talents":
-								case "abilities":
-								case "tags":
-									output[key][chunk].push(data[key]);
-									break;
-
-								default:
-									output[key][chunk][name] = data[key];
-							}
-						}
-					}
-				})
-			}
-		}
-		catch(err) {
-			console.error(err)
-		}
-	})
-	.on("end",() => {
-		let finalOutput = [];
-
-		// TODO weapons
-		// create a JSON file of weapons (array of objects or weapon names), check if any equipment is in that list
-		// if it it, remove from the weapon
-		Object.keys(output).forEach(key => {
-			let adv = output[key];
-
-			// weapons
-			adv.weapons = adv.gear.filter(g => WEAPONS.indexOf(g) != -1);
-
-			adv.gear = adv.gear.filter(g => adv.weapons.indexOf(g) == -1).join(", ");
-			adv.abilities = adv.abilities.map(ab => {
-				if(ab.startsWith("Force Power")) {
-					let key = ab.replace(/[:\s]+/g, "-").toLowerCase();
-					let keyName = key + "-" + adv.name.replace(/\s+/g, "-").replace(/[\(\)]/g, "").toLowerCase();
-
-					if(keyName in abilities) {
-						return abilities[keyName];
-					}
-
-					return abilities[key + "-hobs"];
+					});
 				}
+				else if(outputKeys.indexOf(data.Name.toLowerCase()) != -1) {
+					chunk = data.Name.toLowerCase();
+				}
+				else {
+					Object.keys(data).forEach(key => {
+						if(key != "Name") {
+							if(!(chunk in output[key])) {
+								switch(chunk) {
+									case "gear":
+									case "talents":
+									case "abilities":
+										output[key][chunk] = [];
+										break;
 
-				return (ab in abilities) ? abilities[ab] : ab;
+									// tags key has already been created
+									case "tags":
+										break;
+
+									default:
+										output[key][chunk] = {};
+								}
+							}
+
+							if(data[key]) {
+								let name = data.Name in keyMap ? keyMap[data.Name] : data.Name;
+
+								switch(chunk) {
+									case "gear":
+									case "talents":
+									case "abilities":
+									case "tags":
+										output[key][chunk].push(data[key]);
+										break;
+
+									default:
+										output[key][chunk][name] = data[key];
+								}
+							}
+						}
+					})
+				}
+			}
+			catch(err) {
+				console.error(err)
+			}
+		})
+		.on("end",() => {
+			Object.keys(output).forEach(key => {
+				let adv = output[key];
+
+				// weapons and custom weapons
+				adv.weapons = adv.gear.filter(g => g in customWeapons).map(g => customWeapons[g]);
+				adv.weapons = adv.weapons.concat(adv.gear.filter(g => WEAPONS.indexOf(g) != -1));
+				adv.gear = adv.gear.filter(g => adv.weapons.indexOf(g) == -1).join(", ");
+
+				adv.abilities = adv.abilities.map(ab => {
+					if(ab.startsWith("Force Power")) {
+						let key = ab.replace(/[:\s]+/g, "-").toLowerCase();
+						let keyName = key + "-" + adv.name.replace(/\s+/g, "-").replace(/[\(\)]/g, "").toLowerCase();
+
+						if(keyName in abilities) {
+							return abilities[keyName];
+						}
+
+						return abilities[key + "-hobs"];
+					}
+
+					return (ab in abilities) ? abilities[ab] : ab;
+				});
+
+				adv.derived.defence = [
+				   adv.derived["Melee Defence"],
+				   adv.derived["Ranged Defence"]
+				];
+
+				delete adv.derived["Melee Defence"];
+				delete adv.derived["Ranged Defence"];
+
+				finalOutput.push(adv);
 			});
 
-			adv.derived.defence = [
-			   adv.derived["Melee Defence"],
-			   adv.derived["Ranged Defence"]
-			];
-
-			delete adv.derived["Melee Defence"];
-			delete adv.derived["Ranged Defence"];
-
-			finalOutput.push(adv);
+			complete();
 		});
-
-		console.log(JSON.stringify(finalOutput, null, 1).trim());
-	});
+});
